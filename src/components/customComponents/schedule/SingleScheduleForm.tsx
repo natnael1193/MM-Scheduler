6164817271ed8a51a7f42e82a814bb621e2bc8a1
Natnael Solomon <natnaelsolomon770@gmail.com>
2022-10-24 11:40:20 +0300

    add single schedule added

---
+++ src/components/customComponents/schedule/SingleScheduleForm.tsx
@@ -0,0 +1,117 @@
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
+import React from 'react';
+import { useProgramsQuery } from 'src/services/ProgramApi';
+import { usePriceClassificationsQuery } from 'src/services/PriceClassificationApi';
+import LoadingComponent from '../shared/LoadingComponent';
+import ErrorComponent from '../shared/ErrorComponent';
+
+const SingleScheduleForm = ({ register, handleSubmit, errors }: any) => {
+  const {
+    data: programData,
+    isLoading: programLoading,
+    error: programError,
+  }: any = useProgramsQuery();
+  const {
+    data: priceClassificationData,
+    isLoading: priceClassificationLoading,
+    error: priceClassificationError,
+  }: any = usePriceClassificationsQuery();
+
+  if (programLoading || priceClassificationLoading) return <LoadingComponent />;
+  if (programError || priceClassificationError) return <ErrorComponent />;
+
+  return (
+    <Grid container spacing={4} sx={{ width: '100%', pt: 2 }}>
+      <Grid item lg={4} md={4} sm={12} xs={12}>
+        <TextField label="Alias" {...register('key', { required: true })} fullWidth />
+        <Typography variant="inherit" color="error">
+          {errors.key && 'This is required'}
+        </Typography>
+      </Grid>
+      <Grid item lg={4} md={4} sm={12} xs={12}>
+        <TextField
+          label="Start Time"
+          type="datetime-local"
+          InputLabelProps={{ shrink: true }}
+          inputProps={{ step: 1 }}
+          fullWidth
+          {...register('startTime', { required: true })}
+        />
+        <Typography variant="inherit" color="error">
+          {errors.startTime && 'This is required'}
+        </Typography>
+      </Grid>
+      <Grid item lg={4} md={4} sm={12} xs={12}>
+        <TextField
+          label="End Time"
+          type="datetime-local"
+          InputLabelProps={{ shrink: true }}
+          inputProps={{ step: 1 }}
+          fullWidth
+          {...register('endTime', { required: true })}
+        />
+        <Typography variant="inherit" color="error">
+          {errors.endTime && 'This is required'}
+        </Typography>
+      </Grid>
+      <Grid item lg={6} md={6} sm={12} xs={12}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Program</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Program"
+            {...register('programId', { required: true })}
+            displayEmpty
+            defaultValue={''}
+          >
+            {programData.data.map((programs: any) => {
+              return (
+                <MenuItem value={programs.id} key={programs.id}>
+                  {programs.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+          <Typography variant="inherit" color="error">
+            {errors.programId && 'This is required'}
+          </Typography>
+        </FormControl>
+      </Grid>
+      <Grid item lg={6} md={6} sm={12} xs={12}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Price Classification</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Price Classification"
+            {...register('priceClasifcationId', { required: true })}
+            displayEmpty
+            defaultValue={''}
+          >
+            {priceClassificationData.data.map((priceClassifications: any) => {
+              return (
+                <MenuItem value={priceClassifications.id} key={priceClassifications.id}>
+                  {priceClassifications.name}
+                </MenuItem>
+              );
+            })}
+          </Select>
+          <Typography variant="inherit" color="error">
+            {errors.priceClasifcationId && 'This is required'}
+          </Typography>
+        </FormControl>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SingleScheduleForm;
--- src/pages/customPages/schedule/AddSingleSchedule.tsx
+++ src/pages/customPages/schedule/AddSingleSchedule.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { Card, Grid, Typography, Button } from '@mui/material';
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { useAddSingleScheduleMutation } from 'src/services/ScheduleApi';
+import SingleScheduleForm from '../../../components/customComponents/schedule/SingleScheduleForm';
 
 const AddSingleSchedule = () => {
-  return (
-    <div>AddSingleSchedule</div>
-  )
-}
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-export default AddSingleSchedule
+  const [addSingleSchedule, result] = useAddSingleScheduleMutation();
+
+  useEffect(() => {
+    if (result.isSuccess) {
+      toast.success('Single Schedule Added Successfully');
+      // reset();
+    }
+    if (result.error) {
+      toast.error('Something went wrong!');
+    }
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    addSingleSchedule(data);
+  };
+
+  console.log(result)
+
+  return (
+    <div>
+      <Card sx={{ p: 2 }}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Grid container>
+            <Grid item lg={12} md={12} sm={12} xs={12}>
+              <Typography variant="h3">Add Single Schedule</Typography>
+            </Grid>
+            <SingleScheduleForm {...{ register, handleSubmit, errors }} />
+          </Grid>
+          <Grid item lg={12} md={12} sm={12} xs={12} sx={{ mt: 2 }}>
+            <Button variant="contained" type="submit">
+              {result.isLoading ? 'Loading...' : 'Submit'}
+            </Button>
+          </Grid>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default AddSingleSchedule;
