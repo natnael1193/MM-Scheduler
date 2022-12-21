921a3dc1768d2b44713cf00e69307894269a47c5
Natnael Solomon <natnaelsolomon770@gmail.com>
2022-12-21 13:52:10 +0300

    auth endpoint changed to .env file

--- src/services/LoginApi.tsx
+++ src/services/LoginApi.tsx
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Login } from 'src/interfaces/Login.interface';
 
-const baseURL = `http://localhost:8000/api/Account/authenticate`;
-// const baseURL = `${process.env.REACT_APP_API_SERVER}`;
+// const baseURL = `http://localhost:8000/api/Account/authenticate`;
+const baseURL = `${process.env.REACT_APP_AUTH_API_SERVER}`;
 // const baseToken = `${process.env.REACT_APP_API_TOKEN}`;
 
 export const loginApi = createApi({
@@ -14,7 +14,7 @@ export const loginApi = createApi({
   endpoints: (builder) => ({
     login: builder.mutation<void, Login>({
       query: (spot) => ({
-        url: '/',
+        url: '/Account/authenticate/',
         method: 'POST',
         body: spot,
       }),
